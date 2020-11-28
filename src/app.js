09f07abbdcb5d6e0a300023017beaeb0bddb13f0
tobias <tobias.rocha@hotmail.com>
2020-11-28 02:12:48 -0300

    Rotas finalizadas

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { uuid } = require('uuidv4');
-
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -26,7 +24,7 @@ app.post("/repositories", (request, response) => {
     techs.push(tech);
   });
 
-  const project = { id: uuid(), title: title, url: url, techs: techs, like : 0 };
+  const project = { id: v4(), title: title, url: url, techs: techs, likes : 0 };
 
   projects.push(project);
 
@@ -64,11 +62,23 @@ app.delete("/repositories/:id", (request, response) => {
     return response.status(400).json( {error: 'ID não encontrado'} );
   }
 
-  
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex(project => project.id == id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json( {error: 'ID não encontrado'} );
+  }
+
+  projects[projectIndex].likes = projects[projectIndex].likes + 1;
+
+  return response.json(projects[projectIndex]);
 });
 
 module.exports = app;
